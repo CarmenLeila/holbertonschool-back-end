eb7aa87587d0683f6d19c4b0570526e4d1d980d3
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-04-16 12:49:32 +0200

    correct some indentations

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,15 +8,15 @@ API_URL = "https://jsonplaceholder.typicode.com/"
 
 
 employee_id = sys.argv[1]
-    employee = requests.get(API_URL + "users/{}".format(employee_id)).json()
-    todos = requests.get(API_URL + "todos",
-                         params={"userId": employee_id}).json()
-    done_tasks = [task for task in todos if task.get("completed") is True]
+employee = requests.get(API_URL + "users/{}".format(employee_id)).json()
+todos = requests.get(API_URL + "todos",
+                     params={"userId": employee_id}).json()
+done_tasks = [task for task in todos if task.get("completed") is True]
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee.get("name"), len(done_tasks), len(todos)))
-    for task in done_tasks:
-        print("\t {}".format(task.get("title")))
+print("Employee {} is done with tasks({}/{}):"
+      .format(employee.get("name"), len(done_tasks), len(todos)))
+for task in done_tasks:
+    print("\t {}".format(task.get("title")))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
